9aa3ef398afa5ce8b35f65cbe0570f77a14291a8
Andrew Abletsov <abletsoff@gmail.com>
2024-10-05 12:47:13 +0300

    Exceptions handling

--- auto.sh
+++ auto.sh
@@ -31,22 +31,27 @@ done
 
 echo "Creating result file ($output_filename):"
 readarray -t file_names < <(ls "$output_dir")
-for file_name in "${file_names[@]}"; do
-    if [[ $(echo $file_name | grep -P '\.json$') == '' ]]; then
-        continue
-    fi  
-    output_file="${output_dir}${file_name}"
-    cat "$output_file" | tr '\n' ' ' >> $output_filename 
-    echo -n ',' >> $output_filename
 
-    #if [[ $engagement_id == '' ]]; then
-    #    engagement_id=$(dojo-upload.sh -f $output_file -t "Wpscan API Scan" \
-    #        -p "Casino Website" | grep 'Engagement: ' | cut -d ' ' -f2)
-    #else
-    #    dojo-upload.sh -f $output_file -t "Wpscan API Scan" -p "Casino Website" -e $engagement_id \
-    #        | tail -n 1 | head -c 50
-    #    echo
-    #fi
+for file_name in "${file_names[@]}"; do
+   if [[ $(echo $file_name | grep -P '\.json$') == '' ]]; then
+       continue
+   fi 
+   output_file="${output_dir}${file_name}"
+
+   # If file is not empty (can be due to python script error for some WP isntance)
+   if [[ ! -z "$(cat $output_file)" ]]; then
+   	cat "$output_file" | tr '\n' ' ' >> $output_filename 
+   	echo -n ',' >> $output_filename
+   fi
+
+   #if [[ $engagement_id == '' ]]; then
+   #    engagement_id=$(dojo-upload.sh -f $output_file -t "Wpscan API Scan" \
+   #        -p "Casino Website" | grep 'Engagement: ' | cut -d ' ' -f2)
+   #else
+   #    dojo-upload.sh -f $output_file -t "Wpscan API Scan" -p "Casino Website" -e $engagement_id \
+   #        | tail -n 1 | head -c 50
+   #    echo
+   #fi
 done
 
 output_file_content=$(cat "$output_filename")
